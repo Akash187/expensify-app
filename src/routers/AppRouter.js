2d9b9d4616cc5dcb492823b902268709aa3edf12
Akash Kumar Seth <kumarakashseth007@gmail.com>
2018-02-02 18:25:21 +0530

    Query Strings and URL Parameters

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -15,8 +15,8 @@ const AppRouter = () => (
       <Switch>
         <Route
           path="/" component={ExpenseDashboardPage} exact={true}/>
-        <Route path="/create" activeClassName="is-active" component={AddExpensePage}/>
-        <Route path="/edit" component={EditExpensePage}/>
+        <Route path="/create" component={AddExpensePage}/>
+        <Route path="/edit/:id" component={EditExpensePage}/>
         <Route path="/help" component={HelpPage}/>
         <Route component={NotFoundPage}/>
       </Switch>
